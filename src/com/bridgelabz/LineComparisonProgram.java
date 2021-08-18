507b32b1bbe2066b1b0db0f093a0bc89b0894883
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-18 11:58:20 +0530

    CALCULATION of LENGTH

--- src/com/bridgelabz/LineComparisonProgram.java
+++ src/com/bridgelabz/LineComparisonProgram.java
@@ -25,7 +25,23 @@ public class LineComparisonProgram
         line2.setX2(input.nextInt());
         line2.setY2(input.nextInt());
         System.out.println(" x1 is =" + line2.getX1() + " x2 is = " + line2.getX2() + " y1 = " + line2.getY1() + " y2 = " + line2.getY2());
+
+        double length1 = calculateLength(line1.getX1(), line1.getY1(), line1.getX2(), line1.getY2());
+        System.out.println("length for line1 = " +length1);
+
+        double length2 = calculateLength(line2.getX1(), line2.getY1(), line2.getX2(), line2.getY2());
+        System.out.println("length for line2 = " +length2);
+
     }
+    public static double calculateLength(double x1,double y1,double x2,double y2) //Line1 is assigned
+    {
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return length;
+    }
+
+
+
+
     public static void main(String[] args)
     {
         System.out.println("Welcome to Line-Comparison Program using OOPS ");
